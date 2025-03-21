c69253db4ff8c3d514eac5f62677add830068690
Meta Humus <130368671+meta-human-08@users.noreply.github.com>
2025-03-21 22:45:30 +0530

    Update App.js

--- client/App.js
+++ client/App.js
@@ -3,6 +3,7 @@ import Navigation from './navigation';
 import { store } from './store'
 import { Provider } from 'react-redux'
 export default function App() {
+  console.log("Please commit history")
   return (
     <Provider store={store}>
       <Navigation />
